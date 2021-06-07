7e758584a52f33ee49177416c6cc6f71755731fc
Martín Montanari <martinmontanari3@gmail.com>
2021-06-07 17:41:14 -0300

    server setup

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/',(req, res) =>{
-    res.send('Hola mundo');
+
+app.get('/test',(req, res) =>{
+    res.json(req.body)
 })
+
 
 app.listen(3000, function (){
     console.log('Aplicacion escuchando en el puerto 3000');
